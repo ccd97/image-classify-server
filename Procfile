9fc41b28db9ea6642d0ce8f2dbe8e423cef8def2
CCD <dcunha.cyprien@gmail.com>
2016-11-26 16:38:08 +0530

    setup for deploying to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tf_api.wsgi --log-file -
--- tf_api/settings.py
+++ tf_api/settings.py
@@ -22,9 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6t918%**s8@^(a5kl=y8ir@^goesxbp9#*c46=#hh#dldf8^fu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'deepblue-tensorflow.herokuapp.com',
+]
 
 # Application definition
 
