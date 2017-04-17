e95af944b628d6872b7236dcecf14f440da05ad9
CCD-1997 <dcunha.cyprien@gmail.com>
2017-04-17 21:44:28 +0530

    accept image also in base64 string format

--- classify_image/views.py
+++ classify_image/views.py
@@ -3,6 +3,7 @@ import os
 
 import tensorflow as tf
 from PIL import Image
+from base64 import b64decode
 from django.core.files.temp import NamedTemporaryFile
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -11,11 +12,9 @@ from django.shortcuts import render
 MAX_K = 10
 
 TF_GRAPH = "{base_path}/inception_model/graph.pb".format(
-    base_path=os.path.abspath(os.path.dirname(__file__))
-)
+    base_path=os.path.abspath(os.path.dirname(__file__)))
 TF_LABELS = "{base_path}/inception_model/labels.txt".format(
-    base_path=os.path.abspath(os.path.dirname(__file__))
-)
+    base_path=os.path.abspath(os.path.dirname(__file__)))
 
 
 def load_graph():
@@ -37,19 +36,25 @@ def classify_api(request):
     data = {"success": False}
 
     if request.method == "POST":
+        tmp_f = NamedTemporaryFile()
+
         if request.FILES.get("image", None) is not None:
             image_request = request.FILES["image"]
             image_bytes = image_request.read()
             image = Image.open(io.BytesIO(image_bytes))
-            tmp_file = NamedTemporaryFile()
-            image.save(tmp_file, image.format)
-            classify_result = tf_classify(tmp_file, int(request.POST.get('k', MAX_K)))
-            tmp_file.close()
+            image.save(tmp_f, image.format)
+        elif request.POST.get("image64", None) is not None:
+            base64_data = request.POST.get("image64", None).split(',', 1)[1]
+            plain_data = b64decode(base64_data)
+            tmp_f.write(plain_data)
 
-            if classify_result:
-                data.update({"success": True})
-                for res in classify_result:
-                    data[res[0]] = '{:f}'.format(res[1])
+        classify_result = tf_classify(tmp_f, int(request.POST.get('k', MAX_K)))
+        tmp_f.close()
+
+        if classify_result:
+            data.update({"success": True})
+            for res in classify_result:
+                data[res[0]] = '{:f}'.format(res[1])
 
     return JsonResponse(data)
 
