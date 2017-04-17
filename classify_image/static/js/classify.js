a6383f5499338e5382e3434ebed025c6785ea3b0
CCD-1997 <dcunha.cyprien@gmail.com>
2017-04-17 21:02:52 +0530

    make drag-drop and upload image functional

--- classify_image/static/js/classify.js
+++ classify_image/static/js/classify.js
@@ -1,34 +1,52 @@
 $(document).ready(function() {
 
-    $('.modal').modal({
-        dismissible: false
-    });
+  var dropContainer = document.getElementById('drop-container');
+  dropContainer.ondragover = dropContainer.ondragend = function() {
+    return false;
+  };
 
-    $('#upload-button').click(function() {
-        $('#modal1').modal('open');
-    });
+  dropContainer.ondrop = function(e) {
+    e.preventDefault();
+    loadImage(e.dataTransfer.files[0])
+  }
 
-    $('#go-back, #go-start').click(function() {
-        $(".dd-container").animate({
-            opacity: '1'
-        }, {
-            duration: 200,
-            queue: false
-        });
+  $("#browse-button").change(function() {
+    loadImage($("#browse-button").prop("files")[0]);
+  });
 
-        $(".uf-container").animate({
-            opacity: '0'
-        }, {
-            duration: 200,
-            queue: false
-        });
+  $('.modal').modal({
+    dismissible: false
+  });
 
-        $(".dt-container").animate({
-            opacity: '0'
-        }, {
-            duration: 200,
-            queue: false
-        });
-    });
+  $('#go-back').click(function() {
+    $('#img-card').removeAttr("src");
+    switchCard(0);
+  });
 
+  $('#upload-button').click(function() {
+    $('.modal').modal('open')
+  });
 });
+
+switchCard = function(cardNo) {
+  var containers = [".dd-container", ".uf-container", ".dt-container"];
+  var visibleContainer = containers[cardNo];
+  for (i = 0; i < containers.length; i++) {
+    var oz = (containers[i] === visibleContainer) ? '1' : '0';
+    $(containers[i]).animate({
+      opacity: oz
+    }, {
+      duration: 200,
+      queue: false,
+    }).css("z-index", oz);
+  }
+}
+
+loadImage = function(file) {
+  var reader = new FileReader();
+  reader.onload = function(event) {
+    $('#img-card').attr('src', event.target.result);
+  }
+  reader.readAsDataURL(file);
+  switchCard(1);
+}
